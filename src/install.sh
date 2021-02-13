0f0156578c74ae4abef16186f0f18f1e58e7d18c
Ognian Baruh <ognian@baruh.net>
2021-02-13 15:28:26 +0000

    [Update] Modified install bash script

--- src/install.sh
+++ src/install.sh
@@ -4,7 +4,7 @@ then
 	python3 -m pip install --upgrade pip
 	python3 -m pip install virtualenv
 	python3 -m virtualenv env
-	source env/bin/activate
+	source ./env/bin/activate
 	python3 -m pip install -r requirements.txt
 	python3 -m pip install https://github.com/bitsy-ai/tensorflow-arm-bin/releases/download/v2.4.0-rc2/tensorflow-2.4.0rc2-cp37-none-linux_armv7l.whl
 else
