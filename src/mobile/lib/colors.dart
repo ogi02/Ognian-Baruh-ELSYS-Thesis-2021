3e6e184095e773f6f936b77873a0f36366fc2a91
Ognian Baruh <ognian@baruh.net>
2021-02-15 13:38:27 +0200

    [Add] Added colour scheme

---
+++ src/mobile/lib/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const blue = Color(0xFF0A5687);
+const white = Color(0xFFFFFFFF);
+const green = Color(0xFF6BC7A6);
+const black = Color(0xFF000000);
+const yellow = Color(0xFFFAD161);
+const orange = Color(0xFFF2954E);
