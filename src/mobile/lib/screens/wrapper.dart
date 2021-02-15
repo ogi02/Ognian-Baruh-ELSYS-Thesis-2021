d775e9b2d73f0159c83f5c23acdef19e62f57f32
Ognian Baruh <ognian@baruh.net>
2021-02-15 13:36:10 +0200

    [Add] Created wrapper widget that controls screen based on authentication

--- src/mobile/lib/screens/wrapper.dart
+++ src/mobile/lib/screens/wrapper.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:mobile/screens/authenticate/welcome.dart';
+import 'package:mobile/models/user.dart';
+import 'package:mobile/screens/home/home.dart';
+import 'package:mobile/screens/authenticate/authenticate.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Authenticate();
+
+    final user = Provider.of<User>(context);
+
+    // show either Home or Authentication
+    if (user == null) {
+      return Authenticate();
+    } else {
+      return Home();
+    }
+
   }
 
 }
