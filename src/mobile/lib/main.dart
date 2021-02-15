b71651a1b480f5bf0026d9dc02b7f8b91218a313
Ognian Baruh <ognian@baruh.net>
2021-02-15 13:35:34 +0200

    [Update] Updated main page to call wrapper

--- src/mobile/lib/main.dart
+++ src/mobile/lib/main.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:mobile/screens/authenticate/welcome.dart';
+import 'package:provider/provider.dart';
+import 'package:mobile/models/user.dart';
+import 'package:mobile/services/auth.dart';
+import 'package:mobile/screens/wrapper.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: Authenticate(),
-  ));
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
+    );
+  }
 }
 
 
