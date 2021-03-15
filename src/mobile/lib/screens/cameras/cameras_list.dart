e93da3b9e2ff71d4e9dd9641b40a6ef15d3507f1
Ognian Baruh <ognian@baruh.net>
2021-03-16 01:45:50 +0200

    [Add] Added page for cameras list with components

---
+++ src/mobile/lib/screens/cameras/cameras_list.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:mobile/colors.dart';
+import 'package:mobile/screens/cameras/components/loading_screen.dart';
+import 'package:mobile/services/database.dart';
+
+class CamerasList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _CamerasListState();
+  }
+}
+
+class _CamerasListState extends State<CamerasList> {
+
+  final CloudFirestoreService _dbService = new CloudFirestoreService();
+
+  final String _cameraNameKey = "name";
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: FutureBuilder(
+        future: _dbService.getCameras(),
+        builder: (_, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return LoadingScreen();
+          } else {
+            return ListView.builder(
+                itemCount: snapshot.data.length,
+                itemBuilder: (_, index) {
+                  return Container(
+                    margin: EdgeInsets.only(
+                      top: 6.0,
+                      left: 24.0,
+                      right: 24.0,
+                    ),
+                    padding: EdgeInsets.zero,
+                    child: OutlinedButton(
+                      onPressed: () {},
+                      style: OutlinedButton.styleFrom(
+                        side: BorderSide(
+                          color: blue,
+                          width: 1.6,
+                        ),
+                        padding: EdgeInsets.symmetric(
+                            vertical: 8.0,
+                            horizontal: 6.0
+                        ),
+                      ),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: <Widget>[
+                            Icon(
+                              Icons.camera_alt_outlined,
+                              color: blue,
+                              size: 24.0,
+                            ),
+                            Text(
+                              "  " + snapshot.data[index].data[_cameraNameKey],
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: blue,
+                              ),
+                            ),
+                          ]
+                      ),
+                    ),
+                  );
+                }
+            );
+          }
+        },
+      ),
+    );
+  }
+
+}
