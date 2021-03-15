f54a137d8edeeb066d78ad85b10e59c224e07d05
Ognian Baruh <ognian@baruh.net>
2021-03-16 01:47:09 +0200

    [Add] Init notifications list and notification pages

---
+++ src/mobile/lib/screens/notifications/notifications_list.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class NotificationsList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _NotificationsListState();
+  }
+}
+
+class _NotificationsListState extends State<NotificationsList> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("Notification screen!");
+  }
+
+}
