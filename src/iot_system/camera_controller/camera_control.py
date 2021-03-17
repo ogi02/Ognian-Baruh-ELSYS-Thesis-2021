51b5bc8aedc08c89dd76d1ba2fd98fdc1bf8e8da
Ognian Baruh <ognian@baruh.net>
2021-03-17 12:49:31 +0200

    [Add] Added functionalities for uploading image to cloud storage and timestamp to firestore

---
+++ src/iot_system/camera_controller/camera_control.py
@@ -0,0 +1,73 @@
+# library imports
+from PIL import Image
+from time import time
+from requests import get
+from google.cloud import storage
+from google.cloud import firestore
+
+# image constants
+CAMERA_IP = "172.22.173.47"
+AUTH_USERNAME = "service"
+AUTH_PASSWORD = "Admin!234"
+# IMAGE_FROM_CAMERA_URL = "http://" + CAMERA_IP + "/snap.jpg?JpegCam=1"
+IMAGE_1 = "https://filedn.com/ltOdFv1aqz1YIFhf4gTY8D7/ingus-info/BLOGS/Photography-stocks3/stock-photography-slider.jpg"
+IMAGE_2 = "https://images.unsplash.com/photo-1494253109108-2e30c049369b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTN8fHJhbmRvbXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"
+
+# camera uid
+CAMERA_DEVICE_UID = "da:device:ONVIF:Bosch-FLEXIDOME_IP_4000i_IR-094454407323822009"
+
+# bucket constants
+BUCKET_ID = "iot-home-system-7dab8.appspot.com"
+FOLDER_NAME = "imagesOnDemand"
+
+
+# open image from url
+def open_image_from_url():
+	# retrieve image from url
+	# resp = get(url, auth=HTTPDigestAuth(AUTH_USERNAME, AUTH_PASSWORD), stream=True)
+	resp = get(IMAGE_1, stream=True)
+
+	# return raw image data
+	return resp.content
+
+
+# send image to cloud storage
+def send_image_to_cloud_storage():
+	# get image data
+	image_data = open_image_from_url()
+
+	# init storage client
+	client = storage.Client()
+
+	# get bucket
+	bucket = client.get_bucket(BUCKET_ID)
+
+	# create blob
+	blob = bucket.blob(FOLDER_NAME + "/" + CAMERA_DEVICE_UID + "/image_on_demand.jpg")
+
+	# upload image
+	blob.upload_from_string(
+		image_data,
+		content_type='image/jpg'
+	)
+
+	# send time to firestore
+	send_time_of_image_to_firestore()
+
+# send update message to firestore
+def send_time_of_image_to_firestore():
+	# init firestore client
+	db = firestore.Client()
+
+	# get document
+	doc_ref = db.collection(u'user_devices').document(u'xjMJgd3PP8XuGwlpdrBRnUf0ThC3').collection(u'cameras').document(u'LlI2EAybxztHMtt7ooGq')
+
+	# get now time
+	now = round(time() * 1000)
+
+	# update values
+	doc_ref.update({
+		u'time': now
+	})
+
+send_image_to_cloud_storage()
