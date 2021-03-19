d3dfbaa90a6d9715a7c79747424311983aeebe4f
Ognian Baruh <ognian@baruh.net>
2021-03-19 14:13:35 +0200

    [Add] Added get notification function in mobile app

--- src/mobile/lib/services/database.dart
+++ src/mobile/lib/services/database.dart
@@ -8,6 +8,7 @@ class CloudFirestoreService {
 
   // db constants
   final String _userDevicesCollection = "user_devices";
+  final String _notificationsCollection = "notifications";
   final String _camerasCollection = "cameras";
   final String _locksCollection = "locks";
   final String _userIdKey = "userID";
@@ -43,4 +44,26 @@ class CloudFirestoreService {
     return qn.documents;
   }
 
+  Future getNotifications() async {
+    // get user id from secure storage
+    String userId = await _storage.get(_userIdKey);
+
+    List<String> cameraUids = new List();
+
+    // get all cameras for a certain user
+    await _databaseReference
+        .collection(_userDevicesCollection)
+        .document(userId)
+        .collection(_camerasCollection)
+        .getDocuments()
+        .then((qn) => qn.documents.forEach((doc) => cameraUids.add(doc.data["camera_uid"])));
+
+    // get notifications from the collection of the notification, using the camera uid
+    QuerySnapshot qn = await _databaseReference
+        .collection(_notificationsCollection)
+        .where("camera_uid", whereIn: cameraUids)
+        .getDocuments();
+
+    return qn.documents;
+  }
 }
